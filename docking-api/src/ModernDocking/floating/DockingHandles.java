9415481d981d7e3fea5333671e4ad694b9f81cdf
Andrew Auclair <mightymalakai33@gmail.com>
2023-11-17 19:44:27 -0500

    Cleanup.

--- docking-api/src/ModernDocking/floating/DockingHandles.java
+++ docking-api/src/ModernDocking/floating/DockingHandles.java
@@ -308,45 +308,10 @@ public class DockingHandles {
 	 * @param screenPos New mouse position
 	 */
 	public void update(Point screenPos) {
-		JComponent component = targetRoot;
-
-		Point framePoint = new Point(screenPos);
-		SwingUtilities.convertPointFromScreen(framePoint, component.getParent());
-
-		Point point = (component).getLocation();
-		Dimension size = component.getSize();
-
-		SwingUtilities.convertPointToScreen(point, component.getParent());
-
-//		utilFrame.setLocation(point);
-//		utilFrame.setSize(size);
-
 		setRootHandleLocations();
 		setDockableHandleLocations();
 
-//		int minimumRootHandleDistance = (int) (HANDLE_ICON_SIZE * 1.75);
-//
-//		// check if the root handles happen to be under the dockable handles
-//		if (Math.abs(dockableEast.getX() - rootEast.getX()) < minimumRootHandleDistance &&
-//			Math.abs(dockableEast.getY() - rootEast.getY()) < minimumRootHandleDistance) {
-//			// need to move root east, move it up 1/4 of screen
-//			Point location = targetRoot.getLocation();
-//			Dimension rootSize = targetRoot.getSize();
-//			location.x += rootSize.width / 2;
-//			location.y += (rootSize.height / 2) + (rootSize.height / 4);
-//
-//			SwingUtilities.convertPointToScreen(location, targetRoot.getParent());
-//			SwingUtilities.convertPointFromScreen(location, utilFrame);
-//
-////			setLocation(rootCenter, location.x, location.y);
-////			setLocation(rootWest, location.x - (size.width / 2) + rootHandleSpacing(rootWest), location.y);
-////			setLocation(rootNorth, location.x, location.y - (size.height / 2) + rootHandleSpacing(rootNorth));
-//			setLocation(rootEast, location.x + (rootSize.width / 2) - rootHandleSpacing(rootEast), location.y);
-////			setLocation(rootSouth, location.x, location.y + (size.height / 2) - rootHandleSpacing(rootSouth));
-//
-//		}
-
-		framePoint = new Point(screenPos);
+		Point framePoint = new Point(screenPos);
 		SwingUtilities.convertPointFromScreen(framePoint, utilFrame);
 
 		rootRegion = null;
