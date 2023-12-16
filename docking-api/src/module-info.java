1e9adf0877bcb6de8aadb3b90c2be1cca4b547fb
Andrew Auclair <andrewauclair@users.noreply.github.com>
2023-12-16 10:01:49 -0500

    The internal package is now only exported to the other modern docking modules. (#155)

--- docking-api/src/module-info.java
+++ docking-api/src/module-info.java
@@ -10,8 +10,10 @@ module modern_docking.api {
 	exports ModernDocking.exception;
 	exports ModernDocking.layouts;
 	exports ModernDocking.persist;
- exports ModernDocking.settings;
+    exports ModernDocking.settings;
 	exports ModernDocking.ui;
     exports ModernDocking.api;
-	exports ModernDocking.internal;
+
+	// export our internal package only to our other extension modules
+	exports ModernDocking.internal to modern_docking.ui_ext, modern_docking.single_app, modern_docking.multi_app;
 }
