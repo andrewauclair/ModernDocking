616e5889832d91384f1a83d7a742213d3e2c918e
Andrew Auclair <mightymalakai33@gmail.com>
2024-06-28 22:20:19 -0400

    Adding missing isHidden functions.

--- demo-single-app/src/packets/PacketBytesPanel.java
+++ demo-single-app/src/packets/PacketBytesPanel.java
@@ -73,7 +73,7 @@ public class PacketBytesPanel extends JPanel implements Dockable {
 	}
 
 	@Override
-	public boolean isPinningAllowed() {
+	public boolean isAutoHideAllowed() {
 		return true;
 	}
 
