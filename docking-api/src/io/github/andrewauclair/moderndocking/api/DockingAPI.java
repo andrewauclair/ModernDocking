f5ac8ae5f4e91d64b81e3c17c8c6765fd3fe1894
Andrew Auclair <mightymalakai33@gmail.com>
2025-04-11 17:27:37 -0400

    Issue #308
    Dispose of windows if they are empty after the silent undock within Docking.dock.

--- docking-api/src/io/github/andrewauclair/moderndocking/api/DockingAPI.java
+++ docking-api/src/io/github/andrewauclair/moderndocking/api/DockingAPI.java
@@ -498,6 +498,14 @@ public class DockingAPI {
             DockableWrapper wrapper = internals.getWrapper(source);
 
             wrapper.getParent().undock(source);
+
+            DockingComponentUtils.removeIllegalFloats(this, wrapper.getWindow());
+
+            // dispose the window if we need to
+            if (canDisposeWindow(wrapper.getWindow()) && internals.getRootPanels().get(wrapper.getWindow()).isEmpty() && !Floating.isFloating()) {
+                deregisterDockingPanel(wrapper.getWindow());
+                wrapper.getWindow().dispose();
+            }
         }
 
         DockableWrapper wrapper = internals.getWrapper(target);
