56a6d1b7d66ba25c2c5ed9d7e776ab4500b4b890
Andrew Auclair <mightymalakai33@gmail.com>
2025-05-11 12:55:36 -0400

    Test

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+	`java`
+	`idea`
+	`maven-publish`
+	`signing`
+	id ("com.github.ben-manes.versions") version "0.52.0"
+	id ("io.deepmedia.tools.deployer") version "0.18.0"
+}
+java {
+	sourceCompatibility = JavaVersion.VERSION_11
+	targetCompatibility = JavaVersion.VERSION_11
+}
+subprojects {
+
+
+	repositories {
+		mavenLocal()
+		mavenCentral()
+	}
+
+	version = "1.1.3"
+
+	ext {
+		val flatLafVersion = "3.6"
+	}
+}
+
+//publishing {
+//	repositories {
+//		maven {
+//			name = "OSSRH"
+//			credentials {
+//				username = System.getenv("MAVEN_USERNAME")
+//				password = System.getenv("MAVEN_PASSWORD")
+//			}
+//
+//			url = version.endsWith('SNAPSHOT') ?
+//					'https://central.sonatype.com/repository/maven-snapshots/'
+//					: 'https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/'
+//		}
+//	}
+//}
+
+deployer {
+	content {
+		kotlinComponents()
+	}
+	projectInfo {
+		description = "Modern docking framework for Java Swing"
+		url = "https://github.com/andrewauclair/ModernDocking"
+		scm.fromGithub("andrewauclair", "ModernDocking")
+		license(MIT)
+		developer("Andrew Auclair", "mightymalakai33@gmail.com")
+		groupId = "io.github.andrewauclair.moderndocking"
+	}
+
+	signing {
+		key = secret(System.getenv("MAVEN_GPG_KEY_ID"))
+		password = secret(System.getenv("MAVEN_GPG_PASSPHRASE"))
+	}
+	centralPortalSpec {
+		auth.user = secret(System.getenv("MAVEN_USERNAME"))
+		auth.password = secret(System.getenv("MAVEN_PASSWORD"))
+	}
+}
+//deployer {
+//	project.description = ""
+//
+//	centralPortalSpec("Modern Docking") {
+//		//auth.user = System.getenv("MAVEN_USERNAME")
+//		//auth.password = System.getenv("MAVEN_PASSWORD")
+//
+//
+//		signing {
+//			key = System.getenv("MAVEN_GPG_KEY_ID")
+//			password = System.getenv("MAVEN_GPG_PASSPHRASE")
+//		}
+//	}
+//}
