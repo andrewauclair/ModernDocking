d2a2db8f91b65b4b3e59b4f1a1c0634d586a1d2e
Andrew Auclair <mightymalakai33@gmail.com>
2025-08-03 17:40:20 -0400

    New Theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '1.3.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx_documatt_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_documatt_theme'
 html_static_path = ['_static']
