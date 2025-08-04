9c71ec8ced4c6c0ab76cff9e4aec5698ca2d866e
Andrew Auclair <mightymalakai33@gmail.com>
2025-08-03 20:57:11 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '1.3.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_documatt_theme']
+extensions = ['sphinx_readable_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
